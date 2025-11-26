3fe7854a504e2f0b84800ae20467d3c170146dd1
Snaymi <sneymeb@hotmail.com>
2025-11-26 14:50:39 -0300

    Add dist for GitHub Pages deploy

---
+++ dist/assets/authLayout-BEjVc_Qg.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{h as e}from"./index-BYybgal4.js";const o={};function s(r,a){return e(r.$slots,"default")}const f=t(o,[["render",s]]);export{f as default};
