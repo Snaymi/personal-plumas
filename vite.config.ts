a5c7de2ce4c9383fb0cbb9e75e81f867f2a1104f
Snaymi <98271156+Snaymi@users.noreply.github.com>
2025-12-01 10:33:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
+// vite.config.ts
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/personal-plumas/", // ⚠️ IMPORTANTE
+  base: "/personal-plumas/", // Caminho base para GitHub Pages
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
